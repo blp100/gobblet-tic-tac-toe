5fe0d69296c8b7f6f755f7c147b070a7b0f2abc2
Lin Chen-Hsin <chenhsin@arbo.works>
2023-06-11 12:50:30 +0800

    Revert "feat: rounded corner for grid"
    This reverts commit 39383a79f81aa5d7c50898e9aa4bbc1ed3ba44a1.

--- components/canvas/grid.js
+++ components/canvas/grid.js
@@ -1,28 +1,23 @@
-import { RoundedBox } from "@react-three/drei";
-
 const Grid = (props) => {
   const { color } = props;
-
-  const defaultProps = {
-    castShadow: true,
-    receiveShadow: true,
-    radius: 0.1,
-  };
-
   return (
     <group>
-      <RoundedBox {...defaultProps} position={[-5, 0.5, 0]} args={[1, 1, 30]}>
+      <mesh castShadow receiveShadow position={[-5, 0.5, 0]}>
+        <boxGeometry args={[1, 1, 30]} />
         <meshStandardMaterial color={color} />
-      </RoundedBox>
-      <RoundedBox {...defaultProps} position={[5, 0.5, 0]} args={[1, 1, 30]}>
+      </mesh>
+      <mesh castShadow receiveShadow position={[5, 0.5, 0]}>
+        <boxGeometry args={[1, 1, 30]} />
         <meshStandardMaterial color={color} />
-      </RoundedBox>
-      <RoundedBox {...defaultProps} position={[0, 0.5, 5]} args={[30, 1, 1]}>
+      </mesh>
+      <mesh castShadow receiveShadow position={[0, 0.5, 5]}>
+        <boxGeometry args={[30, 1, 1]} />
         <meshStandardMaterial color={color} />
-      </RoundedBox>
-      <RoundedBox {...defaultProps} position={[0, 0.5, -5]} args={[30, 1, 1]}>
+      </mesh>
+      <mesh castShadow receiveShadow position={[0, 0.5, -5]}>
+        <boxGeometry args={[30, 1, 1]} />
         <meshStandardMaterial color={color} />
-      </RoundedBox>
+      </mesh>
     </group>
   );
 };
