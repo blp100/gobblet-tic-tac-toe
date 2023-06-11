39383a79f81aa5d7c50898e9aa4bbc1ed3ba44a1
Lin Chen-Hsin <chenhsin@arbo.works>
2023-06-11 12:49:15 +0800

    feat: rounded corner for grid

--- components/canvas/grid.js
+++ components/canvas/grid.js
@@ -1,23 +1,28 @@
+import { RoundedBox } from "@react-three/drei";
+
 const Grid = (props) => {
   const { color } = props;
+
+  const defaultProps = {
+    castShadow: true,
+    receiveShadow: true,
+    radius: 0.1,
+  };
+
   return (
     <group>
-      <mesh castShadow receiveShadow position={[-5, 0.5, 0]}>
-        <boxGeometry args={[1, 1, 30]} />
+      <RoundedBox {...defaultProps} position={[-5, 0.5, 0]} args={[1, 1, 30]}>
         <meshStandardMaterial color={color} />
-      </mesh>
-      <mesh castShadow receiveShadow position={[5, 0.5, 0]}>
-        <boxGeometry args={[1, 1, 30]} />
+      </RoundedBox>
+      <RoundedBox {...defaultProps} position={[5, 0.5, 0]} args={[1, 1, 30]}>
         <meshStandardMaterial color={color} />
-      </mesh>
-      <mesh castShadow receiveShadow position={[0, 0.5, 5]}>
-        <boxGeometry args={[30, 1, 1]} />
+      </RoundedBox>
+      <RoundedBox {...defaultProps} position={[0, 0.5, 5]} args={[30, 1, 1]}>
         <meshStandardMaterial color={color} />
-      </mesh>
-      <mesh castShadow receiveShadow position={[0, 0.5, -5]}>
-        <boxGeometry args={[30, 1, 1]} />
+      </RoundedBox>
+      <RoundedBox {...defaultProps} position={[0, 0.5, -5]} args={[30, 1, 1]}>
         <meshStandardMaterial color={color} />
-      </mesh>
+      </RoundedBox>
     </group>
   );
 };
