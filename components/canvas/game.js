30f8a8eff4747e2322119b9f73c32fb135cdc78e
Po-Cheng Yeh <blp100x@hotmail.com>
2023-06-13 14:29:57 +0800

    feat: add Player turn and Gobbler clikcable

--- components/canvas/game.js
+++ components/canvas/game.js
@@ -4,24 +4,30 @@ import Plane from "./plane";
 import Arrow from "./arrow";
 import { SIZES, GOBBLER_TYPE, PLANE_TYPE, PLAYER_INFO } from "../../constants";
 import WinnerText from "./winner-text";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import useStore from "../../store/store";
 import checkGobbler from "../../lib/check-gobbler";
 import checkWinner from "../../lib/check-winner";
 
 const Game = () => {
+  const ref = useRef();
+
+  const activePlayer = useStore((state) => state.activePlayer);
+  const setPlayer = useStore((state) => state.setPlayer);
   const activeGobbler = useStore((state) => state.activeGobbler);
   const activePlane = useStore((state) => state.activePlane);
   const setActiveGobbler = useStore((state) => state.setActiveGobbler);
   const setActivePlane = useStore((state) => state.setActivePlane);
   const board = useStore((state) => state.board);
   const setBoard = useStore((state) => state.setBoard);
+  const winner = useStore((state) => state.winner);
+  const setWinner = useStore((state) => state.setWinner);
 
   useEffect(() => {
     if (activeGobbler && activePlane) {
       const arr = board.get(activePlane.userData.key);
       const newBoard = new Map(board); // create a new board for React state
-      let winner = null;
+      let tempWinner = null;
       if (checkGobbler(arr, activeGobbler)) {
         if (activeGobbler.userData.plane) {
           const planeKey = activeGobbler.userData.plane.userData.key;
@@ -30,61 +36,40 @@ const Game = () => {
           tempArr.splice(prevIndex, 1);
           newBoard.set(planeKey, tempArr); // remove previous gobbler
 
-          if (!winner) {
-            winner = checkWinner(newBoard);
+          if (!tempWinner) {
+            tempWinner = checkWinner(newBoard);
           }
         }
 
         activeGobbler.userData.plane = activePlane;
         const newArr = [...arr, activeGobbler];
         newBoard.set(activePlane.userData.key, newArr); // add gobbler in new position
-        if (!winner) {
-          winner = checkWinner(newBoard);
+        if (!tempWinner) {
+          tempWinner = checkWinner(newBoard);
         }
+        if (tempWinner){
+        setWinner(tempWinner===PLAYER_INFO.PLAYER1.NAME?PLAYER_INFO.PLAYER1:PLAYER_INFO.PLAYER2);
+      }
+
         setActiveGobbler(null);
         setActivePlane(null);
+        setPlayer(
+          activePlayer === PLAYER_INFO.PLAYER1
+            ? PLAYER_INFO.PLAYER2
+            : PLAYER_INFO.PLAYER1
+        );
+        if (tempWinner){
+          setPlayer(null);
+        }
         setBoard(newBoard);
       }
     }
-    // console.log(activeGobbler);
-  }, [activeGobbler, activePlane, board]);
+  }, [activeGobbler, activePlane, activePlayer, board]);
 
-  //   if (state.activeGobbler) {
-  //     const arr = state.board.get(plane.userData.key);
-  //     const newBoard = new Map(state.board); // create a new board for React state
-  //     let winner = null;
-  //     if (checkGobbler(arr, state.activeGobbler)) {
-  //       if (state.activeGobbler.userData.plane) {
-  //         const planeKey = state.activeGobbler.userData.plane.userData.key;
-  //         const tempArr = [...state.board.get(planeKey)];
-  //         const prevIndex = tempArr.indexOf(state.activeGobbler);
-  //         tempArr.splice(prevIndex, 1);
-  //         newBoard.set(planeKey, tempArr); // remove previous gobbler
-
-  //         if (!winner) {
-  //           winner = checkWinner(newBoard);
-  //         }
-  //       }
-  //       state.activeGobbler.userData.plane = plane;
-  //       const newArr = [...arr, state.activeGobbler];
-  //       newBoard.set(plane.userData.key, newArr); // add gobbler in new position
-  //       if (!winner) {
-  //         winner = checkWinner(newBoard);
-  //       }
-
-  //       // console.log(winner);
-  //       return {
-  //         activeGobbler: null,
-  //         board: newBoard,
-  //         activePlayer: PLAYER_INFO.PLAYER2,
-  //       };
-  //     }
-  //   }
-  //   return { activeGobbler: null };
-  // }),
+  // useEffect(() => {
+  // }, [winner]);
 
   // Build gobblers
-
   const gobblers = useMemo(() => {
     const g = [];
     const sizeKeys = Object.keys(SIZES);
@@ -202,8 +187,39 @@ const Game = () => {
     <Scene style={{ height: "100vh" }}>
       {planes}
       {gobblers}
-      {arrows}
-      {winnerTexts}
+      {/* {arrows} */}
+      {/* {winnerTexts} */}
+
+      <Arrow
+        key={PLAYER_INFO.PLAYER1.ARROW_NAME}
+        name={PLAYER_INFO.PLAYER1.ARROW_NAME}
+        color={PLAYER_INFO.PLAYER1.COLOR}
+        xPos={-10}
+        zRot={Math.PI / 2}
+        visible={activePlayer === PLAYER_INFO.PLAYER1}
+      />
+      <Arrow
+        key={PLAYER_INFO.PLAYER2.ARROW_NAME}
+        name={PLAYER_INFO.PLAYER2.ARROW_NAME}
+        color={PLAYER_INFO.PLAYER2.COLOR}
+        xPos={10}
+        zRot={-Math.PI / 2}
+        visible={activePlayer === PLAYER_INFO.PLAYER2}
+      />
+      <WinnerText
+        key={PLAYER_INFO.PLAYER1.WINNER_NAME}
+        name={PLAYER_INFO.PLAYER1.WINNER_NAME}
+        color={PLAYER_INFO.PLAYER1.COLOR}
+        text={"winner!"}
+        visible={winner === PLAYER_INFO.PLAYER1}
+      />
+      <WinnerText
+        key={PLAYER_INFO.PLAYER2.WINNER_NAME}
+        name={PLAYER_INFO.PLAYER2.WINNER_NAME}
+        color={PLAYER_INFO.PLAYER2.COLOR}
+        text={"winner!"}
+        visible={winner === PLAYER_INFO.PLAYER2}
+      />
     </Scene>
   );
 };
